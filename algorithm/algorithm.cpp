d1e76ebdde74bebb730990c859577f9478a1f6c7
帆 高 <1206203790@qq.com>
2021-12-12 15:26:00 +0800

    工作分配问题

--- algorithm/algorithm.cpp
+++ algorithm/algorithm.cpp
@@ -1,23 +1,36 @@
 #include <iostream>
 using namespace std;
 
-void BackTracing() {
+int n;	//n件工作，n个人
+int** c;	//费用数组
+int currc;	//当前费用
+int bestc = 9999;	//费用最优解
+int* x;	//当前工作分配方式
 
-}
-
-
-int main() {
-	int num;
-	cin >> num;
-	bool* visited = new bool[num];
-	for (int i = 0; i < num; i++) visited[i] = false;
-	int** fee = new int* [num];
-	for (int i = 0; i < num; i++) {
-		fee[i] = new int[num];
-		for (int j = 0; j < num; j++) {
-			cin >> fee[i][j];
-		}
+void BackTrack(int t) {
+	if (t == n) {
+		if(currc<bestc) bestc = currc;
+		return;
 	}
 
-	BackTracing();
+	for (int i = t; i < n; i++) {
+		swap(x[i], x[t]);
+		currc += c[t][x[t]];
+		BackTrack(t + 1);
+		currc -= c[t][x[t]];
+		swap(x[i], x[t]);
+	}
+}
+
+int main() {
+	cin >> n;
+	c = new int* [n];
+	x = new int[n];
+	for (int i = 0; i < n; i++) x[i] = i;
+	for (int i = 0; i < n; i++) {
+		c[i] = new int[n];
+		for (int j = 0; j < n; j++) cin >> c[i][j];
+	}
+	BackTrack(0);
+	cout << bestc;
 }
